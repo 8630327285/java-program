2a5322c05a642320519693531795472071a463ba
Nitinkumar <nitinkumar18119@gmail.com>
2024-07-25 15:43:11 +0530

    more codes

---
+++ src/pateern.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class pateern {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("       *");
+        System.out.println("     *");
+        System.out.println("    *");
+        System.out.println(" *");
+        System.out.println("* * * * *");
+    }
+}
---
+++ src/practice/java/twonumber.java
@@ -0,0 +1,9 @@
+package practice.java;
+
+public class twonumber {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 10;
+        System.out.println("sum of " + x + "");
+    }
+}
